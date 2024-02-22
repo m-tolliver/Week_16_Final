97b869ba96b946e172a0958b3d0739907e9dc183
Melinda Tolliver <146542491+m-tolliver@users.noreply.github.com>
2024-02-22 17:21:58 -0500

    created components folder and test components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
     <h1>Best Project Ever!</h1>
+
+    <h3>Name:</h3>
+    <input placeholder='What is your name?' />
     </div>
   );
 }
---
+++ src/Components/test.js
@@ -0,0 +1,12 @@
+function Test() {
+    return (
+      <div className="App">
+      <h1>Best Project Ever!</h1>
+  
+      <h3>Name:</h3>
+      <input placeholder='What is your name?' />
+      </div>
+    );
+  }
+  
+  export default Test;
